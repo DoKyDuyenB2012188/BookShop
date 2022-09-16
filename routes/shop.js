3ed5b5ff6e79f8bf4614ab2c5d50629b141ffc82
dokyduyen <84983247+dokyduyen@users.noreply.github.com>
2022-09-16 23:05:03 +0700

    update2

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,19 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
-const productsController = require('../controllers/products');
-const adminRoutes = require("./admin");
-router.get("/", productsController.getProduct);
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/checkout', shopController.getCheckout);
+
+router.get('/orders', shopController.getOrders)
+
 module.exports = router;
