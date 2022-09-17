17423a5e591fb4ec9d6209d4205db311d983bd06
dokyduyen <84983247+dokyduyen@users.noreply.github.com>
2022-09-17 09:52:53 +0700

    update3

--- models/product.js
+++ models/product.js
@@ -22,6 +22,7 @@ module.exports = class Product {
     this.price = price;
   }
   save() {
+    this.id = Math.random().toString();
     getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -31,5 +32,11 @@ module.exports = class Product {
   }
   static fetchAll(cb) {
     getProductFromFile(cb);
+  }
+  static findById(id, cb) {
+    getProductFromFile(products => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    })
   }
 };
